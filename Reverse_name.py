8a23ada760263d4e7ab3c2aa631fe2aff47fcded
adrian.fundureanu <californicationbot@gmail.com>
2022-12-12 17:09:01 +0200

    fix print

--- Reverse_name.py
+++ Reverse_name.py
@@ -2,5 +2,6 @@ from six.moves import input
 print ("insert ur First name and Last name")
 name = input()
 first = name.upper
-print = ("First Name :"+name.upper)
-print = ("Last Name :"+name.lower)
+print("First Name :"+name.upper)
+print ("Last Name :"+name.lower)
+print
